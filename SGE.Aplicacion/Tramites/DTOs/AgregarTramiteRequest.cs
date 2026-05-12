fd4d62c94f4968b20e1d350122a8c050590c81a5
valentinodellongo <valentinodellongo@gmail.com>
2026-05-12 01:31:26 -0300

    Creacion inicial DTOs tramites

---
+++ SGE.Aplicacion/Tramites/DTOs/AgregarTramiteRequest.cs
@@ -0,0 +1,18 @@
+﻿using SGE.Dominio.Tramites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGE.Aplicacion.Tramites.DTOs
+{
+    internal class AgregarTramiteRequest
+    {
+        public Guid ExpedienteId { get; set; }
+
+        public EtiquetaTramite Etiqueta { get; set; }
+
+        public string Contenido { get; set; }
+
+        public Guid UsuarioId { get; set; }
+    }
+}
