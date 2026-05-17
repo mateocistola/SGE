5bddacc57ca1faf071d918e714fceddcf2d54f00
Rodrigo Napolitano <rodrigojnapolitano19@gmail.com>
2026-05-17 13:50:00 -0300

    Infraestructura completada.

---
+++ SGE.Infraestructura/Autorizacion/AutorizacionProvisionalService.cs
@@ -0,0 +1,11 @@
+﻿using SGE.Aplicacion.Autorizacion;
+
+namespace SGE.Infraestructura.Autorizacion;
+
+public class AutorizacionProvisionalService : IAutorizacionService
+{
+    public bool PoseeElPermiso(Guid idUsuario, Permiso permiso)
+    {
+        return true;
+    }
+}
